d106a093d9f9dc535824a2188830765834d86a0e
george udosen <datameshprojects@gmail.com>
2018-01-23 07:28:13 +0100

    another commit

--- media_devices/js/site.js
+++ media_devices/js/site.js
@@ -1,9 +1,26 @@
 navigator.mediaDevices.getUserMedia({
     audio: false,
     video: true
-}).then(function(stream) {
+}).then (function(stream) {
     var video = document.createElement('video');
     document.body.appendChild(video);
     video.src = URL.createObjectURL(stream);
     video.play();
-})
+});
+
+
+// switch statment
+
+var name = "";
+
+switch (value) {
+    case value1:
+        name += "udosen";
+        break;
+    case value2:
+        name += "udosen";
+        break;
+    default:
+        console.log("Hello World!");
+        break;
+}
