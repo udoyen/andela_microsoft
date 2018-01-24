e0447b450557e2d1932e144a1b45da58246fe8a3
george udosen <datameshprojects@gmail.com>
2018-01-24 03:01:03 +0100

    commit.

---
+++ javascript_best_practice_to_ensure_DOM_is_loaded/js/site.js
@@ -0,0 +1,13 @@
+// To ensure the DOM is ready for use always start you scripts from an init() function
+
+function init(evt) {
+    // Run your codes inside this function
+}
+
+// Another method would be
+
+window.onload = init2;
+
+function init2(evt) {
+    // run code inside here!
+}
